e7e9749f3d2fe79e0822b92d0a36c8caf35d6f37
YSmolders <58876359+YSmolders@users.noreply.github.com>
2020-10-05 15:35:45 +0200

    Update skeleton.ino
    updated with the new code sam and i created on codeshare

--- skeleton.ino
+++ skeleton.ino
@@ -1,14 +1,18 @@
 #include <EEPROM.h>
 #include <Adafruit_NeoPixel.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_pinIO.h>
 
 //Globals, things that cannot be stored locally. Might change when refactoring or when passing params is too slow.
 //Time stuff
 unsigned long lastPoll; //Last time we updated the LEDs
+unsigned long lastDisPoll; 
 unsigned long time; //Current time
 unsigned long rotCWTime; //Last CW rotation detected time
 unsigned long rotCCWTime; //Last CCW rotation detected time
 unsigned long detectTime; //Last time we detected sound and output it
 unsigned long writeTime; //Time at which we detected that we want to write the settings to memory
+unsigned long pushTime;
 
 byte mode; //Which setting to change with a valid range between 0 and modes - 1. Pls no negatives thx.
 int northMax; //Max amplitude detected by the north mic
@@ -18,22 +22,24 @@ int westMax; //Max amplitude detected by the west mic
 bool buttonPushed;
 byte lastDir; //Last direction that we output
 byte settingChanged; //Whether (and which) setting has changed. Default value 255, if anything different then nothing has changed
+const byte noise = 35; //the amount which we say is the very minimun of an actual sound input
 
 //Settings stuff
 byte lowerBound; //The threshold of sounds that we consider to be a sound as opposed to noise. Varies from 0 to 254.
 byte upperBound; //The threshold of what is configured as a loud sound. Varies (after transformation) from 256 to 511.
-byte length; //In seconds, ranging from 0 to 255
+byte soundLength; //In seconds, ranging from 0 to 255
 
 //Constants, things that don't change during execution
-const short pollTime = 100; //Polltime in milliseconds with a valid range between 0 and 32,767. Don't go over and don't go negative pls, thanks.
+const short pollTime = 100; //Polltime in milliseconds (ms) with a valid range between 0 and 32,767. Don't go over and don't go negative pls, thanks.
+const short disPollTime = 400; //Time between display updates in ms with a valid range between 0 and 32,767. Don't go over and don't go negative pls, thanks.
 const byte wTime = 200; //Time needed to wait until we want to write something to memory in ms. Valid values range between 0 and 255
 const byte modes = 3; //The number of settings we can change. May not be larger than 255  though so many settings don't fit in memory either. 
-const byte sensLowerBound = 0; //Address where lower sensitivity bound is stored
-const byte sensUpperBound = 1; //Address where upper sensitivity bound is stored
-const byte soundLength = 2; //Address where length of sound is stored
+const byte sensLowerBound = 3; //Address where lower sensitivity bound is stored
+const byte sensUpperBound = 4; //Address where upper sensitivity bound is stored
+const byte soundLengthAdress = 5; //Address where length of sound is stored
 const byte maxVolDiff = 75; //Maximum difference between two volumes such that they are both considered as loud sounds
-const byte NUMLED = 25; //Amount of LEDs we have
-const byte centralLed = 24;
+const byte NUMLED = 5; //Amount of LEDs we have
+const byte centralLed = 4;
 
 //Pin assignments
 const byte northMic = A0;
@@ -45,50 +51,52 @@ const byte rotPinA = 2;
 const byte rotPinB = 3;
 const byte buttonPin = 4;
 
-/* const int rsPinDisplay = ;
-const int ePinDisplay = ;
-const int d4PinDisplay = ;
-const int d5PinDisplay = ;
-const int d6PinDisplay = ;
-const int d7PinDisplay = ; */
+const int rsPinDisplay = 12;
+const int ePinDisplay = 11;
+const int d4PinDisplay = 10;
+const int d5PinDisplay = 9;
+const int d6PinDisplay = 8;
+const int d7PinDisplay = 7;
 
 const byte ledPin = 5;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMLED, ledPin, NEO_GRB + NEO_KHZ800); //Class needed for the LED strip to work
+hd44780_pinIO lcd(rsPinDisplay, ePinDisplay, d4PinDisplay, d5PinDisplay, d6PinDisplay, d7PinDisplay); //Class needed for the Display to work
 
 void setup(){
-	//Read values from memory
-	lowerBound = EEPROM.read(sensLowerBound);
-  	upperBound = EEPROM.read(sensUpperBound);
-  	length = EEPROM.read(soundLength);
+    //Read values from memory
+    lowerBound = EEPROM.read(sensLowerBound);
+    upperBound = EEPROM.read(sensUpperBound);
+    soundLength = EEPROM.read(soundLengthAdress);
   
-  	if (lowerBound == 255) {
-    	lowerBound = 0;
+    if (lowerBound == 255) {
+        lowerBound = 0;
     }
   
-  	if (upperBound == 255) {
-    	upperBound = 0;
+    if (upperBound == 255) {
+        upperBound = 0;
     }
   
-  	if (length == 255) {
-    	length = 1; //Sound of length 0 seconds is weird, therefore initialise it as 1
+    if (soundLength == 255) {
+        soundLength = 1; //Sound of length 0 seconds is weird, therefore initialise it as 1
     }
   
-  	//Code to initialise rotary encoder
-  	pinMode(rotPinA, INPUT_PULLUP);
-  	pinMode(rotPinB, INPUT_PULLUP);
-  	pinMode(buttonPin, INPUT_PULLUP);
+    //Code to initialise rotary encoder
+    pinMode(rotPinA, INPUT_PULLUP);
+    pinMode(rotPinB, INPUT_PULLUP);
+    pinMode(buttonPin, INPUT_PULLUP);
   
     //Code to initialise mics
     pinMode(northMic, INPUT);
-    pinMode(eastMic, INPUT);
-    pinMode(southMic, INPUT);
-    pinMode(westMic, INPUT);
+    //pinMode(eastMic, INPUT);
+    //pinMode(southMic, INPUT);
+    //pinMode(westMic, INPUT);
 
     //Code to initialise display
+    lcd.begin(16, 2);
 
     //Code to initialise LEDs
-    pinMode(ledPin, OUTPUT);
+    //pinMode(ledPin, OUTPUT);
     strip.begin(); //Initialise LED strip
     strip.show(); //Turn off all LEDs
 
@@ -97,26 +105,27 @@ void setup(){
 
     //Initialise global variables
     lastPoll = 0; 
+    lastDisPoll =0;
     mode = 0;
   
-  	northMax = 0;
-	eastMax = 0;
-	southMax = 0;
-	westMax = 0;
+    northMax = 0;
+    eastMax = 0;
+    southMax = 0;
+    westMax = 0;
   
-	buttonPushed = false;
-  	rotCWTime = 0;
-  	rotCCWTime = 0;
+    buttonPushed = false;
+    rotCWTime = 0;
+    rotCCWTime = 0;
+    pushTime = 0;
     lastDir = 255;
     settingChanged = 255;
   
-  	attachInterrupt(digitalPinToInterrupt(buttonPin), pushDown, FALLING);
+    attachInterrupt(digitalPinToInterrupt(buttonPin), changeMode, FALLING);
     attachInterrupt(digitalPinToInterrupt(rotPinA), checkRotation, FALLING);
 }
 
 void loop() {
     time = millis();
-
     //Get data from mics
     getMicData();
 
@@ -126,10 +135,14 @@ void loop() {
 
         //Change LEDs
         changeLED(dir);
+
+        resetMicData();
+
+        lastPoll  = millis() ;
     }
 
     if (buttonPushed) {
-      	buttonPushed = false; //Reset oushed state and change the mode
+        buttonPushed = false; //Reset pushed state and change the mode
         changeMode();
     }
 
@@ -146,66 +159,101 @@ void loop() {
             break;
 
         case 2:
-            EEPROM.write(soundLength, length);
+            EEPROM.write(soundLengthAdress, soundLength);
 
             break;
         }
 
         settingChanged = 255; //Reset to nothing has changed
     }
-}
-
-void pushDown() {
-	buttonPushed = true;
-}
-
-void getMicData() {
-  	int avg = 512;
-    int north = abs(analogRead(northMic) - avg);
-    int south = abs(analogRead(southMic) - avg);
-    int east = abs(analogRead(eastMic) - avg);
-    int west = abs(analogRead(westMic)- avg);
-
-    if (north > northMax) {
-        northMax = north;
+    
+    if (abs(time - lastDisPoll) > disPollTime) {
+        updateDisplay();
+        lastDisPoll = millis();
     }
 
-    if (south > southMax) {
+}
+
+void updateDisplay() {
+    lcd.clear();
+    switch(mode) {
+      case 0:
+        lcd.print("Lower Sens:");
+        lcd.setCursor(0, 1);
+        lcd.print((float)(lowerBound/255.0) * 100);
+        
+        break;
+      case 1:
+        lcd.print("Upper Sens:");
+        lcd.setCursor(0, 1);
+        lcd.print((float)(upperBound/255.0) * 100);
+        
+        break;
+      case 2:
+        lcd.print("Sound Length:");
+        lcd.setCursor(0, 1);
+        lcd.print(soundLength);
+        
+        break;
+    }
+}
+
+
+void getMicData() {
+    int avg = 512;
+    int north = abs(analogRead(northMic) - avg);
+    //int south = abs(analogRead(southMic) - avg);
+    //int east = abs(analogRead(eastMic) - avg);
+    //int west = abs(analogRead(westMic)- avg);
+
+    if (north > northMax && north > noise) {
+        northMax = north;
+    }
+    /*
+    if (south > southMax && south > noise) {
         southMax = south;
     }
 
-    if (east > eastMax) {
+    if (east > eastMax && east > noise) {
         eastMax = east;
     }
 
-    if (west > westMax) {
+    if (west > westMax && west > noise) {
         westMax = west;
     }
+    */
+}
+
+void resetMicData(){
+    northMax = 0;
+    eastMax = 0;
+    southMax = 0;
+    westMax = 0;
 }
 
 int calculateDirection() {
     //Calculate the direction given the data from the mics, outpute a value between 0 (north) and 7 (north west), following the compass in the N, E, S, W order
-	int neDiff = abs(northMax - eastMax);
-  	int nwDiff = abs(northMax - westMax);
-  	int seDiff = abs(southMax - eastMax);
-  	int swDiff = abs(southMax - westMax);
+    int neDiff = abs(northMax - eastMax);
+    int nwDiff = abs(northMax - westMax);
+    int seDiff = abs(southMax - eastMax);
+    int swDiff = abs(southMax - westMax);
   
-  	if (neDiff < maxVolDiff && neDiff > westMax && neDiff > southMax) {
-    	return 1;
+    if (neDiff < maxVolDiff && neDiff > westMax && neDiff > southMax) {
+        return 1;
     } else if (seDiff < maxVolDiff && seDiff > westMax && seDiff > northMax) {
-    	return 3;
+        return 3;
     } else if (swDiff < maxVolDiff && swDiff > eastMax && swDiff > northMax) {
-    	return 5;
+        return 5;
     } else if (nwDiff < maxVolDiff && nwDiff > eastMax && nwDiff > southMax) {
-    	return 7;
+        return 7;
     } else if (northMax > southMax && northMax > eastMax && northMax > westMax) {
-    	return 0;
+        return 0;
     } else if (eastMax > southMax && eastMax > northMax && eastMax > westMax) {
-    	return 2;
+        return 2;
     } else if (southMax > northMax && southMax > eastMax && southMax > westMax) {
-    	return 4;
+        return 4;
     } else if (westMax > southMax && westMax > eastMax && westMax > northMax) {
-    	return 6;
+        return 6;
     }
 }
 
@@ -213,28 +261,34 @@ void changeLED(int dir) {
     //Assuming LEDs are connected per direction, so 0 1 2 are the north facing LEDs, 3 4 5 the north east facing LEDs, etc.
     unsigned long color = strip.Color(0, 255, 0); //Green
 
-    if (lastDir == dir && time > detectTime + (length * 1000)) {
+    if (lastDir == dir && time > detectTime + (soundLength * 1000)) {
         color = strip.Color(255, 0, 0); //Red
     } else if (lastDir != dir) {
         detectTime = millis();
         lastDir = dir;
     }
-
-    strip.setPixelColor(dir*3, color);
-    strip.setPixelColor((dir*3) + 1, color);
-    strip.setPixelColor((dir*3) + 2, color);
+    
+    int max = 0;
 
     switch (dir) {
     case 0:
         if (northMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
         }
+        
+        max = northMax;
 
         break;
 
     case 1:
         if (northMax > (upperBound + 255) || eastMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
+        }
+        
+        if (northMax > eastMax) {
+            max = northMax;
+        } else {
+            max = eastMax;
         }
 
         break;
@@ -243,12 +297,20 @@ void changeLED(int dir) {
         if (eastMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
         }
+        
+        max = eastMax;
 
         break;
 
     case 3:
         if (eastMax > (upperBound + 255) || southMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
+        }
+        
+        if (southMax > eastMax) {
+            max = southMax;
+        } else {
+            max = eastMax;
         }
 
         break;
@@ -257,12 +319,20 @@ void changeLED(int dir) {
         if (southMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
         }
+        
+        max = southMax;
 
         break;
 
     case 5:
         if (southMax > (upperBound + 255) || westMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
+        }
+        
+        if (southMax > westMax) {
+            max = southMax;
+        } else {
+            max = westMax;
         }
 
         break;
@@ -271,12 +341,20 @@ void changeLED(int dir) {
         if (westMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
         }
+        
+        max = westMax;
 
         break;
 
     case 7:
         if (northMax > (upperBound + 255) || westMax > (upperBound + 255)) {
             strip.setPixelColor(centralLed, strip.Color(255, 0, 0));
+        }
+        
+        if (northMax > westMax) {
+            max = northMax;
+        } else {
+            max = westMax;
         }
 
         break;
@@ -285,54 +363,77 @@ void changeLED(int dir) {
         break;
     }
 
+    if (max < lowerBound) {
+        strip.setPixelColor(dir*3, 0, 0, 0); 
+    } else {
+        strip.setPixelColor(dir*3, color);
+    }
+
+    if (max > (2*255)/3) {
+        strip.setPixelColor((dir*3) + 1, color);    
+    } else {
+        strip.setPixelColor((dir*3) + 1, 0, 0, 0);
+    }
+  
+    if (max > (4*255) / 3) {
+        strip.setPixelColor((dir*3) + 2, color);
+    } else {
+        strip.setPixelColor((dir*3) + 2, 0, 0, 0);   
+    }
+
     strip.show(); //Update LEDs
 }
 
 void changeMode() {
-    if (mode + 1 > modes) {
-        mode = 0;
-    } else {
-        mode += 1;
+    if(abs(pushTime-time) < 500){
+        return;
     }
+    mode++;
+    if(mode >= modes){
+        mode = 0;
+    }
+    pushTime=millis();
 }
 
 void checkRotation() {
-	int valA = digitalRead(rotPinA);
-  	int valB = digitalRead(rotPinB);
+    int valA = digitalRead(rotPinA);
+    int valB = digitalRead(rotPinB);
   
-  	if (valA == valB && (time > rotCWTime + 50)) {
-    	increase();
-    	rotCWTime = time;
-  	}
+    if (valA == valB && (time > rotCWTime + 50)) {
+        increase();
+        rotCWTime = time;
+    }
   
-  	if (valA =! valB && (time > rotCCWTime + 50)) {
-    	decrease();
-    	rotCCWTime = time;
-  	}
+    if (valA != valB && (time > rotCCWTime + 50)) {
+        decrease();
+        rotCCWTime = time;
+    }
+
 }
 
 void increase() {
-    writeTime = millis();
+    writeTime = wTime + millis();
     switch(mode) {
         case 0:
-      	    if (lowerBound != 254) {
-        	    lowerBound++;
+            if (lowerBound != 254) {
+                lowerBound++;
                 settingChanged = mode;
+                
             }
 
             break;
 
         case 1:
-    	    if (upperBound != 254) {
-        	    upperBound++;
+            if (upperBound != 254) {
+                upperBound++;
                 settingChanged = mode;
             }
 
             break;
 
         case 2:
-            if (length != 254) {
-                length++;
+            if (soundLength != 254) {
+                soundLength++;
                 settingChanged = mode;
             }
 
@@ -347,6 +448,7 @@ void decrease() {
             if (lowerBound != 0) {
                 lowerBound--;
                 settingChanged = mode;
+                Serial.println(lowerBound);
             }
 
             break;
@@ -360,8 +462,8 @@ void decrease() {
             break;
 
         case 2:
-            if (length != 1) {
-                length--;
+            if (soundLength != 1) {
+                soundLength--;
                 settingChanged = mode;
             }
 
